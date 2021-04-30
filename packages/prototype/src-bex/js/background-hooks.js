05019af2329f795587365e53b952b84db57270d8
ArsalaBangash <arsalabangash@gmail.com>
2021-04-30 07:39:15 +0500

    fix: multiple command listeners now not registered

--- packages/prototype/src-bex/js/background-hooks.js
+++ packages/prototype/src-bex/js/background-hooks.js
@@ -1,39 +1,52 @@
-// Hooks added here have a bridge allowing communication between the BEX Background Script and the BEX Content Script.
-// Note: Events sent from this background script using `bridge.send` can be `listen`'d for by all client BEX bridges for this BEX
+/* Hooks added here can communicate between the BEX Background & Content Scripts
+More info: https://quasar.dev/quasar-cli/developing-browser-extensions/background-hooks
+*/
+const focusMode = { "twitter": true, "linkedin": true };
 
-// More info: https://quasar.dev/quasar-cli/developing-browser-extensions/background-hooks
+var isCommandListenerRegistered = false
 
-export default function attachBackgroundHooks (bridge /* , allActiveConnections */) {
-  var focusMode = {"twitter": true, "linkedin": true};
-  var currentURL = null;
-  
+export default function attachBackgroundHooks(bridge /* , allActiveConnections */) {
+  console.log(`INFO: Is Command listener registered? : ${isCommandListenerRegistered}`)
 
-  chrome.commands.onCommand.addListener(function(command , tab) {
+  registerCommandListener(bridge)
+}
 
-    chrome.tabs.get(tab.id, function(tab){
-      currentURL = tab.url;
+function registerCommandListener(bridge) {
+  console.log(`INFO: Is Command listener registered? : ${isCommandListenerRegistered}`)
+
+  if (isCommandListenerRegistered) {
+    console.log("WARNING: Command listener already registered, skipping")
+    return
+  }
+
+  function toggleFocusListener(command, tab) {
+
+    chrome.tabs.get(tab.id, function (tab) {
+      const currentURL = tab.url;
       console.log(currentURL)
 
-      if (currentURL == "https://twitter.com/home") {
+      if (currentURL === "https://twitter.com/home") {
         console.log("sending message to twitter")
-        sendStatus(focusMode, "twitter", bridge)
-      }else if (currentURL == "https://www.linkedin.com/feed/") {
+        toggleFocus("twitter", bridge)
+      } else if (currentURL === "https://www.linkedin.com/feed/") {
         console.log("sending message to linkedin")
-        sendStatus(focusMode, "linkedin", bridge)
+        toggleFocus("linkedin", bridge)
       }
 
     });
-  });
+  }
 
+  chrome.commands.onCommand.addListener(toggleFocusListener);
+
+  isCommandListenerRegistered = true
+  console.log("SUCCESS: Command listener registered")
 }
 
-
-function sendStatus(focusMode, webPage, bridge){
-  if (focusMode[webPage]){
+function toggleFocus(webPage, bridge) {
+  if (focusMode[webPage]) {
     bridge.send("focus")
-  }else{
+  } else {
     bridge.send("un-focus")
   }
   focusMode[webPage] = !focusMode[webPage]
 }
-
