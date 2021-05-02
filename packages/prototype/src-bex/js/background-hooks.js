c9a5d378e9c512499ef2168897723f9db3342f68
daveavi <avidave97@gmail.com>
2021-05-01 23:05:37 -0400

    Very Close to finishing v0.9, we have default focus mode working

--- packages/prototype/src-bex/js/background-hooks.js
+++ packages/prototype/src-bex/js/background-hooks.js
@@ -1,12 +1,27 @@
 /* Hooks added here can communicate between the BEX Background & Content Scripts
 More info: https://quasar.dev/quasar-cli/developing-browser-extensions/background-hooks
 */
-const focusMode = { "twitter": true, "linkedin": true };
+var focusMode = {"twitter": {"focus": false, "initial": true}, "linkedin":{"focus": false, "initial": true} };
 
 var isCommandListenerRegistered = false
 
 export default function attachBackgroundHooks(bridge /* , allActiveConnections */) {
-  registerCommandListener(bridge)
+
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    var tab = tabs[0];
+    var url = tab.url;
+    console.log(focusMode)
+    if (url === "https://twitter.com/home") {
+      initializeFocus("twitter",bridge)
+    } else if (url === "https://www.linkedin.com/feed/") {
+      initializeFocus("linkedin",bridge)
+    }
+
+    registerCommandListener(bridge)
+  });
 }
 
 function registerCommandListener(bridge) {
@@ -40,11 +55,41 @@ function registerCommandListener(bridge) {
   console.log("SUCCESS: Command listener registered")
 }
 
+
+
 function toggleFocus(webPage, bridge) {
-  if (focusMode[webPage]) {
-    bridge.send("focus")
+  if (focusMode[webPage].focus) {
+    sendFocus(webPage,bridge)
   } else {
-    bridge.send("un-focus")
+    sendUnFocus(webPage,bridge)
   }
-  focusMode[webPage] = !focusMode[webPage]
+  focusMode[webPage].focus = !focusMode[webPage].focus
+}
+
+
+function sendFocus(webPage,bridge){
+  bridge.send("focus", {page: webPage }).then(response => {
+    console.log('Successfully focused on ' + webPage)
+  })
+}
+
+function sendUnFocus(webPage,bridge){
+  bridge.send("un-focus", {page: webPage }).then(response => {
+    console.log('Successfully un focused on ' + webPage)
+  })
+}
+
+
+function initializeFocus(webPage,bridge){
+  var initializeFocus = focusMode[webPage].initial || !focusMode[webPage].focus
+  if(initializeFocus){
+    console.log("initializing focus")
+    sendFocus(webPage,bridge)
+    console.log(focusMode)
+  }
+
+  if (focusMode[webPage].initial){
+    focusMode[webPage].initial = !focusMode[webPage].initial
+    console.log(focusMode)
+  }
 }
--- packages/prototype/src-bex/js/content-hooks.js
+++ packages/prototype/src-bex/js/content-hooks.js
@@ -20,27 +20,36 @@ const
 
 
 const currentUrl = window.location.href
-console.log(currentUrl)
 
+var hidden = false; 
+var initialLoad = true;
 export default function attachContentHooks (bridge) {
-  // handle event
+    // handle event
   bridge.on('focus', function (event) {
-    if (currentUrl == "https://twitter.com/home"){
+    console.log("for event focus")
+    let currentUrl = event.data.page
+
+
+    if (currentUrl == "twitter"){
       console.log("about to enter focus on Twitter")
-      hideTwitter(true)
+      blockFeedAndPanelTwitter()
     }
-    else if(currentUrl == "https://www.linkedin.com/feed/"){
+    else if(currentUrl == "linkedin"){
       console.log("about to focus on linkedin")
       hideLinkedIn(true)
     }
   })
 
   bridge.on('un-focus', function (event) { 
-    if (currentUrl == "https://twitter.com/home"){
+    console.log("for event un-focus")
+    console.log(event)
+    let currentUrl = event.data.page
+
+    if (currentUrl == "twitter"){
       console.log("about to un-focus on Twitter")
-      hideTwitter(false)
+      hideTwitterDistractions(false)
     }
-    else if(currentUrl == "https://www.linkedin.com/feed/"){
+    else if(currentUrl == "linkedin"){
       console.log("about to un-focus on linkedin")
       hideLinkedIn(false)
     } 
@@ -76,41 +85,96 @@ function createIframe () {
 
 
 function hideLinkedIn(hide){
-  if(hide){
-    // const newsFeedContainer = document.getElementsByClassName(NEWS_FEED_CLASSNAME)[0]
-    // document.getElementsByClassName(SHARED_NEWS_CLASSNAME)[0].style.visibility = 'hidden'
-    // for (let i = 0; i < newsFeedContainer.children.length; i++) {
-    //     newsFeedContainer.children[i].style.visibility = 'hidden';
-    // }
-    // document.getElementsByClassName('ad-banner-container is-header-zone ember-view')[0].style.visibility = 'hidden'
-    // document.getElementsByClassName(LINKEDIN_NEWS)[0].style.visibility = 'hidden'
-    // displayQuote()
-    // setTimeout(() => {
-    //     document.getElementsByClassName(MAIN_CONTAINER_CLASSNAME)[0].style.opacity = "1"
-    // }, 148)
-    document.getElementsByClassName(HOME_PAGE_CLASS)[0].style.visibility = "hidden"
-  }else{
-    // document.getElementsByClassName(SHARED_NEWS_CLASSNAME)[0].style.visibility = 'visible'
-    // document.getElementsByClassName(NEWS_FEED_CLASSNAME)[0].children[0].remove()
-    // for (let i = 0; i < newsFeedContainer.children.length; i++) {
-    //     newsFeedContainer.children[i].style.visibility = 'visible';
-    // }
-    // document.getElementsByClassName('ad-banner-container is-header-zone ember-view')[0].style.visibility = 'visible'
-    // document.getElementsByClassName(LINKEDIN_NEWS)[0].style.visibility = 'visible'
-    document.getElementsByClassName(HOME_PAGE_CLASS)[0].style.visibility = "visible"
+  try {
+    if(hide){
+      document.getElementsByClassName(HOME_PAGE_CLASS)[0].style.visibility = "hidden"
+    }else{
+      document.getElementsByClassName(HOME_PAGE_CLASS)[0].style.visibility = "visible"
+    }
+  }catch(err) {
+    console.log(err)
+  }
+
+}
+
+var hidden = false
+function hideTwitterDistractions(hide){
+  try { 
+    if(hide){
+      document.querySelectorAll('[role="main"]')[0].children[0].children[0].children[0].children[0].children[0].children[3].style.visibility = "hidden"
+      document.querySelectorAll('[role="main"]')[0].children[0].children[0].children[0].children[1].children[0].children[1].children[0].children[0].children[0].children[2].style.visibility = "hidden"
+      hidden = true;
+    }else{
+      document.querySelectorAll('[role="main"]')[0].children[0].children[0].children[0].children[0].children[0].children[3].style.visibility = "visible"
+      document.querySelectorAll('[role="main"]')[0].children[0].children[0].children[0].children[1].children[0].children[1].children[0].children[0].children[0].children[2].style.visibility = "visible"
+    }
+  }catch(err){
+    console.log(err)
   }
 }
 
 
-function hideTwitter(hide){
-  if(hide){
-    document.getElementsByClassName(TWITTER_FEED_CLASS)[1].style.visibility = "hidden"
-    document.getElementsByClassName(TWITTER_PANEL_CLASS)[0].style.visibility = "hidden"
-  }else{
-    document.getElementsByClassName(TWITTER_FEED_CLASS)[1].style.visibility = "visible"
-    document.getElementsByClassName(TWITTER_PANEL_CLASS)[0].style.visibility = "visible"
+var intervalId;
+function tryBlockingTwitterHome() {
+  if (distractionsHidden(true)) {
+      clearInterval(intervalId)
+  } else {
+      try {
+          if (!hidden) {
+            hideTwitterDistractions(true);
+          }
+      } catch (err) {
+          console.log("Feed hasn't been loaded yet");
+      }
+
   }
 }
+
+function blockFeedAndPanelTwitter() {
+  if (homePageTwitterHasLoaded()) {
+    hideTwitterDistractions(true)
+  } else {
+      if(initialLoad){
+          intervalId = setInterval(tryBlockingTwitterHome, 1000)
+          initialLoad =  false;
+      }else{
+          intervalId = setInterval(tryBlockingTwitterHome, 100)
+      }
+  }
+}
+
+function distractionsHidden(isHomePage) {
+  if (isHomePage) {
+      if (feedHasLoaded()) {
+          return document.getElementsByClassName(TWITTER_PANEL_CLASS)[0].style.visibility == "hidden" && document.getElementsByClassName(TWITTER_FEED_CLASS)[1].style.visibility == "hidden";
+      }
+      return false;
+  } else {
+      if (panelHasLoaded()) {
+          return document.getElementsByClassName(TWITTER_PANEL_CLASS)[0].style.visibility == "hidden"
+      } 
+      return false;
+  }
+}
+
+
+function homePageTwitterHasLoaded() {
+  return panelHasLoaded() && feedHasLoaded();
+}
+
+
+
+
+function panelHasLoaded() {
+  return  document.getElementsByClassName(TWITTER_FEED_CLASS)[1]
+}
+
+function feedHasLoaded() {
+  return document.getElementsByClassName(TWITTER_PANEL_CLASS)[0]
+}
+
+
+
 
 ;(function () {
   // When the page loads, insert our browser extension code.
