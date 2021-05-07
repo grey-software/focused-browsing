5cc4415e13604c59df82cfe95d1c1d00e297d4ed
ArsalaBangash <arsalabangash@gmail.com>
2021-05-08 01:10:13 +0500

    Refactors

--- packages/prototype/src-bex/js/content-hooks.js
+++ packages/prototype/src-bex/js/content-hooks.js
@@ -3,191 +3,198 @@
 var TWITTER_FEED_CLASS = "";
 var TWITTER_PANEL_CLASS = "";
 
-const FEED_CONTAINER_CLASS_NAME = "section[aria-labelledby^=accessible-list]"
-const NEWS_FEED_CLASSNAME = "core-rail"
-const SHARED_NEWS_CLASSNAME = "ad-banner-container artdeco-card ember-view"
-const MAIN_CONTAINER_CLASSNAME = "neptune-grid three-column ghost-animate-in"
-const LINKEDIN_NEWS = "news-module pv3 ember-view"
+const FEED_CONTAINER_CLASS_NAME = "section[aria-labelledby^=accessible-list]";
+const NEWS_FEED_CLASSNAME = "core-rail";
+const SHARED_NEWS_CLASSNAME = "ad-banner-container artdeco-card ember-view";
+const MAIN_CONTAINER_CLASSNAME = "neptune-grid three-column ghost-animate-in";
+const LINKEDIN_NEWS = "news-module pv3 ember-view";
 
-const HOME_PAGE_CLASS = "self-focused ember-view"
+const DEFAULT_FRAME_HEIGHT = "100px";
+const DEFAULT_FRAME_WIDTH = "120px";
 
-const
-  iFrame = document.createElement('iframe'),
-  defaultFrameHeight = '100px',
-  defaultFrameWidth = '120px'
+const VISIBILITY_HIDDEN = "hidden";
+const VISIBILITY_VISIBLE = "visible";
 
+const HOME_PAGE_CLASS = "self-focused ember-view";
+var appIframe;
 
+const currentUrl = window.location.href;
 
-
-const currentUrl = window.location.href
-
-var hidden = false; 
+var areDistractionsHidden = false;
 var initialLoad = true;
-export default function attachContentHooks (bridge) {
-    // handle event
-  bridge.on('focus', function (event) {
-    console.log("for event focus")
-    let currentUrl = event.data.page
 
+(function () {
+  // When the page loads, insert our browser extension code.
+  initIframe();
+  // document.getElementsByClassName(TWITTER_FEED_CLASS)[1].append(iFrame)
+})();
 
-    if (currentUrl == "twitter"){
-      console.log("about to enter focus on Twitter")
-      blockFeedAndPanelTwitter()
-      //I am appending it to the body just to test it out for now, to see if the code is being rendered on focus right away 
-      document.body.prepend(iFrame)
+export default function attachContentHooks(bridge) {
+  // handle event
+  bridge.on("focus", function (event) {
+    console.log("for event focus");
+    let currentUrl = event.data.page;
+
+    if (currentUrl == "twitter") {
+      console.log("about to enter focus on Twitter");
+      startIframe();
+      focusTwitter();
+    } else if (currentUrl == "linkedin") {
+      console.log("about to focus on linkedin");
+      hideLinkedIn(true);
     }
-    else if(currentUrl == "linkedin"){
-      console.log("about to focus on linkedin")
-      hideLinkedIn(true)
+  });
+
+  bridge.on("un-focus", function (event) {
+    console.log("for event un-focus");
+    console.log(event);
+    let currentUrl = event.data.page;
+
+    if (currentUrl == "twitter") {
+      console.log("about to un-focus on Twitter");
+      toggleTwitterDistractions(false);
+    } else if (currentUrl == "linkedin") {
+      console.log("about to un-focus on linkedin");
+      hideLinkedIn(false);
     }
-  })
-
-  bridge.on('un-focus', function (event) { 
-    console.log("for event un-focus")
-    console.log(event)
-    let currentUrl = event.data.page
-
-    if (currentUrl == "twitter"){
-      console.log("about to un-focus on Twitter")
-      hideTwitterDistractions(false)
-    }
-    else if(currentUrl == "linkedin"){
-      console.log("about to un-focus on linkedin")
-      hideLinkedIn(false)
-    } 
-  })
-
-
-}
-
-
-const setIFrameDimensions = (height, width) => {
-  iFrame.height = height
-  iFrame.width = width
-  document.body.style.paddingLeft = width
+  });
 }
 
 // create iframe
-function createIframe () {
-  setIFrameDimensions(defaultFrameHeight, defaultFrameWidth)
-  Object.assign(iFrame.style, {
-    position: 'fixed',
-    border: 'none',
-    zIndex: '10000'
-  })
-
-  iFrame.src = chrome.runtime.getURL('www/index.html')
+function initIframe() {
+  appIframe = document.createElement("iframe");
+  console.log(appIframe);
+  //I am appending it to the body just to test it out for now, to see if the code is being rendered on focus right away
 }
 
+function startIframe() {
+  appIframe.src = chrome.runtime.getURL("www/index.html");
+  console.log(appIframe);
+}
 
+function injectIframe() {
+  appIframe.width = DEFAULT_FRAME_WIDTH;
+  appIframe.height = DEFAULT_FRAME_HEIGHT;
+  // document.body.style.paddingLeft = width;
+  Object.assign(appIframe.style, {
+    position: "fixed",
+    border: "none",
+    zIndex: "10000",
+  });
+  document.body.prepend(appIframe);
+  console.log(appIframe);
+}
 
-function hideLinkedIn(hide){
+function hideLinkedIn(hide) {
   try {
-    if(hide){
-      document.getElementsByClassName(HOME_PAGE_CLASS)[0].style.visibility = "hidden"
-    }else{
-      document.getElementsByClassName(HOME_PAGE_CLASS)[0].style.visibility = "visible"
+    if (hide) {
+      document.getElementsByClassName(
+        HOME_PAGE_CLASS
+      )[0].style.visibility = VISIBILITY_HIDDEN;
+    } else {
+      document.getElementsByClassName(
+        HOME_PAGE_CLASS
+      )[0].style.visibility = VISIBILITY_VISIBLE;
     }
-  }catch(err) {
-    console.log(err)
-  }
-
-}
-
-var hidden = false
-function hideTwitterDistractions(hide){
-  try { 
-    if(hide){
-      document.getElementsByClassName(TWITTER_FEED_CLASS)[0].style.visibility = "hidden"
-      document.getElementsByClassName(TWITTER_PANEL_CLASS)[1].style.visibility = "hidden"
-
-  
-      hidden = true;
-    }else{
-      document.getElementsByClassName(TWITTER_FEED_CLASS)[0].style.visibility = "visible"
-      document.getElementsByClassName(TWITTER_PANEL_CLASS)[1].style.visibility = "visible"
-    }
-  }catch(err){
-    console.log(err)
+  } catch (err) {
+    console.log(err);
   }
 }
 
+var areDistractionsHidden = false;
+function toggleTwitterDistractions(shouldHide) {
+  try {
+    if (shouldHide) {
+      document.getElementsByClassName(
+        TWITTER_FEED_CLASS
+      )[0].style.visibility = VISIBILITY_HIDDEN;
+      document.getElementsByClassName(
+        TWITTER_PANEL_CLASS
+      )[1].style.visibility = VISIBILITY_HIDDEN;
+      injectIframe();
+      areDistractionsHidden = true;
+    } else {
+      document.getElementsByClassName(
+        TWITTER_FEED_CLASS
+      )[0].style.visibility = VISIBILITY_VISIBLE;
+      document.getElementsByClassName(
+        TWITTER_PANEL_CLASS
+      )[1].style.visibility = VISIBILITY_VISIBLE;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 var intervalId;
 function tryBlockingTwitterHome() {
   if (distractionsHidden()) {
-      clearInterval(intervalId)
+    clearInterval(intervalId);
   } else {
-      try {
-          if (!hidden) {
-            hideTwitterDistractions(true);
-          }
-      } catch (err) {
-          console.log("Feed hasn't been loaded yet");
+    try {
+      if (!areDistractionsHidden) {
+        toggleTwitterDistractions(true);
       }
-
+    } catch (err) {
+      console.log("Feed hasn't been loaded yet");
+    }
   }
 }
 
-function blockFeedAndPanelTwitter() {
+function focusTwitter() {
   if (homePageTwitterHasLoaded()) {
-    hideTwitterDistractions(true)
+    toggleTwitterDistractions(true);
   } else {
-      if(initialLoad){
-          intervalId = setInterval(tryBlockingTwitterHome, 1000)
-          initialLoad =  false;
-      }else{
-          intervalId = setInterval(tryBlockingTwitterHome, 100)
-      }
+    if (initialLoad) {
+      intervalId = setInterval(tryBlockingTwitterHome, 1000);
+      initialLoad = false;
+    } else {
+      intervalId = setInterval(tryBlockingTwitterHome, 100);
+    }
   }
 }
 
 function distractionsHidden() {
-    if (feedHasLoaded()) {
-      return isTwitterFeedHidden() && isTwitterPanelHidden()
-    }
-
+  if (feedHasLoaded()) {
+    return isTwitterFeedHidden() && isTwitterPanelHidden();
+  }
 }
-
 
 function homePageTwitterHasLoaded() {
   return panelHasLoaded() && feedHasLoaded();
 }
 
-
-function isTwitterFeedHidden(){
-  return document.getElementsByClassName(TWITTER_FEED_CLASS)[0].style.visibility == "hidden"
+function isTwitterFeedHidden() {
+  return (
+    document.getElementsByClassName(TWITTER_FEED_CLASS)[0].style.visibility ==
+    VISIBILITY_HIDDEN
+  );
 }
 
-function isTwitterPanelHidden(){
-  return document.getElementsByClassName(TWITTER_PANEL_CLASS)[1].style.visibility == "hidden"
+function isTwitterPanelHidden() {
+  return (
+    document.getElementsByClassName(TWITTER_PANEL_CLASS)[1].style.visibility ==
+    VISIBILITY_HIDDEN
+  );
 }
 
 function panelHasLoaded() {
-  TWITTER_FEED_CLASS = getTwitterPanelClassName()
-  return TWITTER_FEED_CLASS
+  TWITTER_FEED_CLASS = getTwitterPanelClassName();
+  return TWITTER_FEED_CLASS;
 }
 
 function feedHasLoaded() {
-  TWITTER_PANEL_CLASS = getTwitterFeedClassName()
-  return TWITTER_PANEL_CLASS
+  TWITTER_PANEL_CLASS = getTwitterFeedClassName();
+  return TWITTER_PANEL_CLASS;
 }
 
-
-
-function getTwitterFeedClassName(){
-  return document.querySelectorAll('[role="main"]')[0].children[0].children[0].children[0].children[0].children[0].children[3].className
+function getTwitterFeedClassName() {
+  return document.querySelectorAll('[role="main"]')[0].children[0].children[0]
+    .children[0].children[0].children[0].children[3].className;
 }
 
-function getTwitterPanelClassName(){
-  return document.querySelectorAll('[role="main"]')[0].children[0].children[0].children[0].children[1].children[0].children[1].children[0].children[0].children[0].children[2].className
+function getTwitterPanelClassName() {
+  return document.querySelectorAll('[role="main"]')[0].children[0].children[0]
+    .children[0].children[1].children[0].children[1].children[0].children[0]
+    .children[0].children[2].className;
 }
-
-
-
-;(function () {
-  // When the page loads, insert our browser extension code.
-  createIframe()  
-  // document.body.prepend(iFrame)
-  // document.getElementsByClassName(TWITTER_FEED_CLASS)[1].append(iFrame)
-})()
--- packages/prototype/src/pages/Index.vue
+++ packages/prototype/src/pages/Index.vue
@@ -1,14 +1,203 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+  <div class="focus-container">
+    <div class="focus-card-container">
+      <div class="focus-card">
+        <div class="title">
+          <div class="focused-browsing-logi"></div>
+          <p class="focused-browsing-is-active">Focused Browsing is active</p>
+        </div>
+        <p class="amet-minim-mollit-non-deserunt-ullamco-e">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit.
+        </p>
+        <div class="buttons">
+          <div class="disable-two">
+            <p class="disable">Disable</p>
+          </div>
+          <div class="more-info">
+            <p class="read-docs">Read Docs</p>
+          </div>
+        </div>
+        <div class="project-info">
+          <p class="support-this-project">Support this project</p>
+          <p class="support-this-project">Why are you in Focused Mode</p>
+        </div>
+      </div>
+    </div>
+    <div class="quote">
+      <p class="amet-minim-mollit-non-deserunt-ullamco">
+        <strong class="amet-minim-mollit-non-deserunt-ullamco-emphasis-0"
+          >“ </strong
+        >Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequat duis enim velit mollit.<strong
+          class="amet-minim-mollit-non-deserunt-ullamco-emphasis-0"
+        >
+          ”</strong
+        >
+      </p>
+      <p class="probably-someone-year">- probably someone, year !&#64;#%</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
-}
+  name: "FocusContainer",
+};
 </script>
+
+<style lang="scss" scoped>
+$black: rgba(0, 0, 0, 1);
+$black-2: rgba(19, 19, 19, 1);
+$gray: rgba(144, 144, 144, 1);
+
+// Typographies
+@mixin inter-12-medium {
+  font-family: "Inter";
+  font-size: 12px;
+  font-weight: 500;
+  line-height: 18px;
+}
+@mixin inter-14-medium {
+  font-family: "Inter";
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 21px;
+}
+@mixin inter-14-regular {
+  font-family: "Inter";
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 21px;
+}
+@mixin inter-14-semi-bold {
+  font-family: "Inter";
+  font-size: 14px;
+  font-weight: 600;
+  line-height: 21px;
+}
+@mixin inter-18-medium {
+  font-family: "Inter";
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 27px;
+}
+
+.focus-container {
+  background-color: rgba(255, 255, 255, 1);
+  padding: 57px 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.focus-card-container {
+  background-color: rgba(255, 255, 255, 1);
+  margin-bottom: 61px;
+  border-radius: 16px;
+  padding: 49px 58px;
+  box-shadow: 0 4px 12px 0 rgba(19, 19, 19, 0.15);
+  display: flex;
+  align-items: center;
+}
+.focus-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.title {
+  margin-bottom: 8px;
+  display: flex;
+  align-items: center;
+}
+.focused-browsing-logi {
+  margin-right: 7px;
+  padding: 1.54px 1px 1.53px;
+  display: flex;
+  align-items: center;
+}
+.focused-browsing-is-active {
+  color: $black;
+  letter-spacing: -0.27px;
+  @include inter-18-medium;
+}
+.amet-minim-mollit-non-deserunt-ullamco-e {
+  height: 60px;
+  width: 264px;
+  color: $gray;
+  display: flex;
+  text-align: center;
+  margin-bottom: 28px;
+  letter-spacing: -0.21px;
+  @include inter-14-regular;
+}
+.buttons {
+  margin-bottom: 36px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.disable-two {
+  margin-bottom: 15px;
+  border-radius: 6px;
+  padding: 9px 104px 8px;
+  display: flex;
+  align-items: center;
+  border: 1px solid $black-2;
+}
+.disable {
+  color: $black-2;
+  letter-spacing: -0.21px;
+  @include inter-14-semi-bold;
+}
+.more-info {
+  background-color: $black-2;
+  border-radius: 6px;
+  padding: 10px 94px 9px 95px;
+  display: flex;
+  align-items: center;
+}
+.read-docs {
+  color: rgba(254, 254, 254, 1);
+  letter-spacing: -0.21px;
+  @include inter-14-semi-bold;
+}
+.project-info {
+  display: flex;
+  align-items: flex-start;
+}
+.support-this-project {
+  color: rgba(135, 135, 135, 1);
+  text-align: center;
+  text-decoration: underline;
+  letter-spacing: -0.18px;
+  @include inter-12-medium;
+  &:not(:last-of-type) {
+    margin-right: 37px;
+  }
+}
+.quote {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+.amet-minim-mollit-non-deserunt-ullamco {
+  height: 38px;
+  width: 432px;
+  display: flex;
+  text-align: center;
+  margin-bottom: 12px;
+  color: $gray;
+  @include inter-14-regular;
+}
+.amet-minim-mollit-non-deserunt-ullamco-emphasis-0 {
+  color: $black-2;
+  letter-spacing: -0.21px;
+  @include inter-14-regular;
+}
+.probably-someone-year {
+  color: $black;
+  margin-left: 216px;
+  letter-spacing: -0.21px;
+  @include inter-14-medium;
+}
+</style>
