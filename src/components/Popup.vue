c69e23c48bd3332d08931554f8d6313fe8a2bc88
daveavi <avidave97@gmail.com>
2021-06-11 12:45:25 -0400

    improved popup ui

---
+++ src/components/Popup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="frame-32">
+    <div class="frame-5">
+      <p class="focused-browsing">Focused Browsing</p>
+    </div>
+    <img
+      alt=""
+      class="illustration-yoga"
+      src="https://static.overlay-tech.com/assets/73171884-5b27-41f9-81bd-bceb2a06f0ec.svg"
+    /><img
+      alt=""
+      class="vector-3"
+      src="https://static.overlay-tech.com/assets/d9a91395-5e2c-4708-8bc8-c90ae85d9fe4.svg"
+    />
+    <p class="press-ctrl-shift-1-to-show-or-hi">
+      Press
+      <strong
+        class="press-ctrl-shift-1-to-show-or-hi-emphasis-1"
+        >Ctrl / </strong
+      ><strong
+        class="press-ctrl-shift-1-to-show-or-hi-emphasis-2"
+        >⌘</strong
+      ><strong
+        class="press-ctrl-shift-1-to-show-or-hi-emphasis-3"
+      >
+      </strong
+      ><strong
+        class="press-ctrl-shift-1-to-show-or-hi-emphasis-1"
+        >&#43; Shift &#43; 1</strong
+      >
+      to show or hide the feed.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Popup"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,12 @@ mix.setPublicPath('./')
     .sass('src/sass/twitter.scss', 'extension-build/css')
     .sass('src/sass/twitter-dim.scss','extension-build/css')
     .sass('src/sass/twitter-dark.scss', 'extension-build/css')
+    .sass('src/sass/popup.scss', 'extension-build/css')
     .js('src/js/background.js', 'extension-build/js')
     .js('src/js/content.js', 'extension-build/js')
     .js('src/js/Twitter/TwitterController.js', 'extension-build/js/Twitter')
     .js('src/js/feed-card-controller.js', 'extension-build/js').vue()
+    .js('src/js/popup-card-controller.js', 'extension-build/js').vue()
     .options({
         processCssUrls: false
     })
