c425783edcbf38b814350aaf5e3ba012108351b1
daveavi <avidave97@gmail.com>
2021-06-21 22:10:33 -0400

    execute script is working now

---
+++ src/focus.js
@@ -0,0 +1,5 @@
+import LinkedInController from './js/LinkedIn/LinkedInController'
+const ACTION = "focus"
+console.log(currentURL)
+// let linkedInController = new LinkedInController()
+// linkedInController.handleActionOnPage(URL, ACTION)
