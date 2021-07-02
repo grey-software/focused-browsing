95ddc4474ebc5c72681dd61076c22a0872bb1263
daveavi <avidave97@gmail.com>
2021-07-01 23:29:24 -0400

    one more commit

--- src/components/MainCard/MainCard.vue
+++ src/components/MainCard/MainCard.vue
@@ -1,30 +1,33 @@
 <template>
-  <div class="card-main">
+<div class="card-main">
       <div class="card-content">
-          <div class="card-title">
-            <CardHeading  />
-            <p class="card-subtitle">
-              We&#039;ve hidden this website&#039;s
-              distracting elements so you can focus on your
-              creative and professional goals.
-            </p>
-            <ButtonGroup />
-            <div class="keyboard-shortcut">
-              <p class="caption">
-                Or press <strong>Ctrl/⌘&#43;Shift&#43;1 </strong> to show/hide
-                feed
-              </p>
-            </div>
-          </div>
+        <div class="title-cointainer">
+          <CardHeading/>
+          <p class="card-subtitle">
+            We&#039;ve hidden this website&#039;s
+            distracting elements so you can focus on your
+            creative and professional goals.
+          </p>
+        </div>
+        <ButtonGroup />
       </div>
-  </div>
+      <div class="shortcut-container">
+        <p class="shortcut-caption">
+          Or
+          <strong class="shortcut-caption-emphasis-1"
+            >Shift &#43; F &#43; B</strong
+          ><strong class="shortcut-caption-emphasis-2">
+          </strong
+          > to show/hide the feed
+        </p>
+      </div>
+    </div>
 </template>
 
 <script>
-import CardHeading from './Heading/CardHeading.vue'
-import ButtonGroup from './ButtonGroup/ButtonGroup.vue'
-
-export default {
+import CardHeading from './CardHeading.vue'
+import ButtonGroup from './ButtonGroup.vue'
+ export default {
   name: "MainCard",
   components: { CardHeading, ButtonGroup}
 };
