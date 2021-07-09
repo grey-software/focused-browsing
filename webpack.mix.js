51e629d9c43df5905edf1b7e30331c9ece05d350
daveavi <avidave97@gmail.com>
2021-07-09 10:21:31 -0400

    adding commits with changes to permissions (taking out activeTab) and adding inter into our packages

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,9 +3,10 @@ let mix = require("laravel-mix")
 mix.copy('src/www', 'extension-build/www')
 mix.copy('src/manifest.json', 'extension-build')
 mix.copy('src/icons','extension-build/icons')
-
+mix.copy('src/sass/font-files','extension-build/css/font-files')
 
 mix.setPublicPath('./')
+    .sass('src/sass/inter.scss', 'extension-build/css')
     .sass('src/sass/linkedin.scss', 'extension-build/css')
     .sass('src/sass/twitter.scss', 'extension-build/css')
     .sass('src/sass/twitter-dim.scss','extension-build/css')
