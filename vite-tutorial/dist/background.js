dd71474a5f59a4fa6bf0d8b0da5e0f448e9caa0c
daveavi <avidave97@gmail.com>
2021-08-03 17:26:22 -0400

    deleted files that are not needed anymore

--- vite-tutorial/dist/background.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./assets/vendor.3d5ea50e.js";e.browserAction.onClicked.addListener((async s=>{console.log("executing script"),await e.tabs.executeScript(s.id,{file:"focus.js",runAt:"document_start"})})),e.runtime.onMessage.addListener((function(e){console.log(e)}));
--- vite-tutorial/dist/focus.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"./assets/vendor.3d5ea50e.js";!async function(){console.log("focus.ts injected");const s=(await o.tabs.query({active:!0,currentWindow:!0}))[0];console.log(s)}();
